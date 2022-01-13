51e88ac3f7109c2b89c7aa0869f8e1392f027297
Kevin Duff <kevinkelduff@gmail.com>
2022-01-13 23:39:21 +0000

    Fix u.s.a and similar words appearing

--- src/wordle_api/words.py
+++ src/wordle_api/words.py
@@ -1,4 +1,5 @@
 from functools import lru_cache
+from string import ascii_lowercase
 
 from english_words import english_words_lower_alpha_set
 
@@ -8,4 +9,9 @@ def candidate_words(size: int):
     """
     Get English words of a particular size
     """
-    return sorted(word for word in english_words_lower_alpha_set if len(word) == size)
+    possible_words = {
+        word
+        for word in english_words_lower_alpha_set
+        if all(letter in ascii_lowercase for letter in word)
+    }
+    return sorted(word for word in possible_words if len(word) == size)
